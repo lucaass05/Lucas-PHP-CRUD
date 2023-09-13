b5c76854d26e9c9734fb45a71a9a17677f94a2df
tobiane <42877632+tobiane@users.noreply.github.com>
2023-09-13 15:13:29 -0300

    acabe

--- index.php
+++ index.php
@@ -5,21 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <h1>Menu general</h1>
-    Formularios
-    <ul>
-        <a href="#">Altas</a>
-        <a href="#">Bajas</a>
-        <a href="#">Modificacion</a>
-        <a href="#">Listar</a>
-        <a href="#">Menu</a>
+<body style="font-family: Verdana, Geneva, Tahoma, sans-serif;">
+<h1>Menu general</h1>
+Formularios
+<ul>
+<a href="forms/form-altas.php">Alta</a><br>
+	<a href="forms/form-bajas.php">Bajas</a><br>
+	<a href="forms/form-modificacion.php">Modificaciones</a><br>
+	<a href="forms/listar.php">Listado de personas</a><br>
+    <a href="forms/menu.php">Menu</a><br>
     </ul>
     Funcionalidad
     <ul>
-        <a href="">Altas</a>
-        <a href="">Bajas</a>
-        <a href="">Modificacion</a>
+    <a href="funcionalidad/altas.php">Altas</a><br>
+    <a href="funcionalidad/bajas.php">Bajas</a><br>
+    <a href="funcionalidad/modificacion.php">modificacion</a><br>
     </ul>
 </body>
 </html>
